6da5a79934bc70935be8af50aa266250cc96827c
Bella <text_me2@protonmail.com>
2018-03-20 21:50:45 +0100

    Impossible de se connecter avec les id créé en base de donnée

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -15,9 +15,8 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  */
 class User implements UserInterface
 {
-    public function __construct()
-    {
-        $this->salt = md5(uniqid('', true));
+    function __construct() {
+        return $this->salt = md5(uniqid(mt_rand(), true));
     }
 
     /**
