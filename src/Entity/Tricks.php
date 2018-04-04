e69ea2be08fef03c1a557a27eb955f72a5b63503
Bella <text_me2@protonmail.com>
2018-04-04 22:29:00 +0200

    le parametre de l'attribut $image modifié

--- src/Entity/Tricks.php
+++ src/Entity/Tricks.php
@@ -17,39 +17,96 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class Tricks
 {
+    /********** ATTRIBUTS **********/
+
     /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * Nom de la figure
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * La description de la figure
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @var Collection
      * @ORM\OneToMany(targetEntity="App\Entity\Image", mappedBy="tricks", cascade={"persist", "remove"})
-     * @Assert\Count(
-     *      min = 1,
-     *      max = 5,
-     *      minMessage = "tricks.images.min",
-     *      maxMessage = "tricks.images.max"
-     * )
      * @ORM\JoinColumn(nullable=true)
-     * @Assert\Valid()
      */
     private $images;
 
-
-
     /**
-     * @param mixed $images
+     * Video représentant la figure
+     * @ORM\Column(type="simple_array", name="video", nullable=true)
      */
-    public function setImages($images): void
-    {
-        $this->images = $images;
-    }
+    private $videos;
 
     /**
      * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="tricks", cascade={"persist", "remove"})
      */
     private $comments;
 
+    /**
+     * Auteur de la figure
+     * * @ORM\Column(type="string", length=255, name="user")
+     */
+    private $user;
+
+    /**
+     * Date de la creation de la figure
+     * @ORM\Column(name="date", type="datetime")
+     */
+    private $date;
+
+    /**
+     * Le groupe de la figure
+     * @ORM\Column(type="string", length=255)
+     */
+    private $groupe;
 
     /**
      * @ORM\Column(name="updated_at", type="datetime", nullable=true)
      */
     private $updatedAt;
+
+
+    /********** CONSTRUCTOR **********/
+
+    public function __construct()
+    {
+        $this->date = new \Datetime();
+        $this->images = new ArrayCollection();
+        $this->comments = new ArrayCollection();
+    }
+
+
+    /********** GETTERS & SETTERS **********/
+
+    /**
+     * @return Collection
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    /**
+     * @param Collection $images
+     */
+    public function setImages(Collection $images): void
+    {
+        $this->images = $images;
+    }
 
     /**
      * @return mixed
@@ -68,27 +125,130 @@ class Tricks
     }
 
     /**
+     * @return Collection|Comment[]
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGroupe()
+    {
+        return $this->groupe;
+    }
+
+    /**
+     * @param mixed $groupe
+     */
+    public function setGroupe($groupe)
+    {
+        $this->groupe = $groupe;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getVideos()
+    {
+        return $this->videos;
+    }
+
+    /**
+     * @param mixed $videos
+     */
+    public function setVideos($videos): void
+    {
+        $this->videos = $videos;
+    }
+
+
+    /********** AUTRES METHODES **********/
+
+    /**
      * @ORM\PreUpdate
      */
     public function updateDate()
     {
         $this->setUpdatedAt(new \Datetime());
     }
-    //constructeur des figures
-
-    public function __construct()
-    {
-        $this->date = new \Datetime();
-        $this->images = new ArrayCollection();
-
-        $this->comments = new ArrayCollection();
-
-    }
 
     /**
      * Add image
-     *
-     *
      * @param Image $image
      * @return Tricks
      */
@@ -110,207 +270,5 @@ class Tricks
     {
         $this->images->removeElement($image);
     }
-    /**
-     * Get images
-     *
-     * @return \Doctrine\Common\Collections\Collection
-     */
-    public function getImages()
-    {
-        return $this->images;
-    }
-
-
-
-
-
-
-    /**
-     * @return Collection|Comment[]
-     */
-    public function getComments()
-    {
-        return $this->comments;
-    }
-
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @return mixed
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param mixed $id
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-    }
-
-
-    /**
-     * Nom de la figure
-     * @ORM\Column(type="string", length=255)
-     */
-    private $name;
-
-    /**
-     * @return mixed
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * @param mixed $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-
-
-
-
-    /**
-     * La description de la figure
-     * @ORM\Column(type="text")
-     */
-
-    private $description;
-
-    /**
-     * @return mixed
-     */
-    public function getDescription()
-    {
-        return $this->description;
-    }
-
-    /**
-     * @param mixed $description
-     */
-    public function setDescription($description)
-    {
-        $this->description = $description;
-    }
-
-
-
-
-
-    /**
-     * Le groupe de la figure
-     * @ORM\Column(type="string", length=255)
-     */
-    private $groupe;
-
-    /**
-     * @return mixed
-     */
-    public function getGroupe()
-    {
-        return $this->groupe;
-    }
-
-    /**
-     * @param mixed $groupe
-     */
-    public function setGroupe($groupe)
-    {
-        $this->groupe = $groupe;
-    }
-
-
-
-
-    /**
-     * Date de la creation de la figure
-     * @ORM\Column(name="date", type="datetime")
-     */
-    private $date;
-
-    /**
-     * @return mixed
-     */
-    public function getDate()
-    {
-        return $this->date;
-    }
-
-    /**
-     * @param mixed $date
-     */
-    public function setDate($date)
-    {
-        $this->date = $date;
-    }
-
-
-
-    /**
-     * Auteur de la figure
-     * * @ORM\Column(type="string", length=255, name="user")
-     */
-    private $user;
-
-    /**
-     * @return mixed
-     */
-    public function getUser()
-    {
-        return $this->user;
-    }
-
-    /**
-     * @param mixed $user
-     */
-    public function setUser($user)
-    {
-        $this->user = $user;
-    }
-
-
-
-
-
-
-    /**
-     * Video représentant la figure
-     * @ORM\Column(type="simple_array", name="video", nullable=true)
-     */
-    private $videos;
-
-    /**
-     * @return mixed
-     */
-    public function getVideos()
-    {
-        return $this->videos;
-    }
-
-    /**
-     * @param mixed $videos
-     */
-    public function setVideos($videos): void
-    {
-        $this->videos = $videos;
-    }
-
-
-
-
-
 
 }
